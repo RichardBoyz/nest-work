c665bdc56e621909505a7a5a46c8fffc1ce84747
richardboyz <r1382333@gmail.com>
2022-08-02 21:20:48 +0800

    Set test module Product

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
+    ProductsModule,
     MongooseModule.forRoot(
       'mongodb+srv://r1382333:r8654321@cluster0.ttwoxij.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
     ),
