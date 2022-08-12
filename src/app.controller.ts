1cdc01ca602b474a1b7a8a001e0b355652f8df1a
richardboyz <r1382333@gmail.com>
2022-08-13 01:03:23 +0800

    stash

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Post,
+  Request,
+  Response,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthenticatedGuard } from './auth/authenticated.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -10,8 +20,24 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @UseGuards(AuthenticatedGuard)
   @Get('/test')
-  getTest(): string {
-    return 'test1';
+  getTest(@Request() req): string {
+    return req.user;
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Request() req): any {
+    return { message: '登入成功!' };
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Post('logout')
+  logout(@Request() req, @Response() res): string {
+    return req.logout((err) => {
+      if (err) throw new BadRequestException('登出失敗ㄏㄏ');
+      res.send({ message: 'ㄅㄅ' });
+    });
   }
 }
