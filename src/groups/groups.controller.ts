7f42dd846229fecb5735e28f7d9bd5500778dbb2
richardboyz <r1382333@gmail.com>
2023-05-13 11:38:01 +0800

    Add create group

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { GroupsService } from './groups.service';
+import { Group } from './groups.model';
 
 @Controller('groups')
-export class GroupsController {}
+export class GroupsController {
+  constructor(private readonly groupService: GroupsService) {}
+
+  @Post()
+  async createGroup(@Body('group') group: Group) {
+    const groupId = await this.groupService.create(group);
+    return groupId;
+  }
+}
--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Group } from './groups.model';
 @Injectable()
-export class GroupsService {}
+export class GroupsService {
+  constructor(
+    @InjectModel('Group') private readonly groupModel: Model<Group>,
+  ) {}
+
+  async create(group: Group) {
+    const newGroup = new this.groupModel(group);
+    const result = await newGroup.save();
+    return result.id;
+  }
+}
